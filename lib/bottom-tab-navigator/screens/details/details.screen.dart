0cf9fe86045323072d01b329029f6a68d3c0221d
Danillo Oliveira <danillo.alves.o@gmail.com>
2024-07-30 21:13:05 -0300

    chore: autosave configs

--- lib/bottom-tab-navigator/screens/details/details.screen.dart
+++ lib/bottom-tab-navigator/screens/details/details.screen.dart
@@ -1,9 +1,8 @@
-import 'package:boilerplate/bottom-tab-navigator/routes/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 class DetailsScreen extends StatelessWidget {
-  const DetailsScreen({ super.key });
+  const DetailsScreen({super.key});
 
   void navigate(BuildContext context) {
     context.pop();
@@ -12,14 +11,17 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Details Screen")
-      ),
+      appBar: AppBar(title: const Text("Details Screen")),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
+        children: <Widget>[
           const Center(child: Text("Details Screen")),
-          ElevatedButton(onPressed: (){ navigate(context); }, child: const Text("GO BACK!"))
+          ElevatedButton(
+            onPressed: () {
+              navigate(context);
+            },
+            child: const Text("GO BACK!"),
+          )
         ],
       ),
     );
